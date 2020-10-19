6f0221923d49755ffe026ed899bb18d1dc28b1fa
ram333n <71777440+ram333n@users.noreply.github.com>
2020-10-19 18:43:46 +0300

    Add files via upload

---
+++ lab_1_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+//вважатимемо, що x,y,z-натуральні числа.Якщо ж розв'язувати задачу для цілих чисел, то можна дописати "+ or -" до числа
+void PyphagorasNumbers() {
+    int n;
+    cout << "Enter the n" << endl;
+    cin >> n;
+    for (int x = 1; x < n; x++) {
+        for (int y = 1; y < n; y++) {
+            for (int z = 1; z <= n; z++) {
+                int SumOfQuadratic = x * x + y * y;
+                int ZQuadric = z * z;
+                if (SumOfQuadratic == ZQuadric) cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
+            }
+        }
+    }
+}
+int main()
+{
+    PyphagorasNumbers();
+}
---
+++ lab_1_2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+const int LEN = 128;
+//визначаємо останній індекс
+//a[i]==0 
+int getLastIdx(char* a) {
+	for (int i = 0; i < LEN; i++) {
+		if (a[i] == 0) return i-1;
+	}
+	return LEN;
+}
+//параметри за вказівником(масиви)
+void sum(char* a, char* b, char* sum) {
+
+	int aIdx = getLastIdx(a);
+	int bIdx = getLastIdx(b);
+	//визначаємо індекс суми
+	int sumIdx = (aIdx > bIdx ? aIdx : bIdx) + 1;
+	int c = 0;
+	//код '0' = 48; '1'=49,... , 96 = 2 відступи, віднімаємо 96 для того, щоб отримати числове значення суми розрядів у int . c- перенесення 
+	while (aIdx > -1 && bIdx > -1) {
+		int s = (a[aIdx] + b[bIdx] - 96 + c);
+		sum[sumIdx] = (s % 10) + 48;
+		c = s / 10;
+		aIdx--, bIdx--, sumIdx--;
+	}
+	//якщо є перенесення, то нічого не відбувається(залишається в int).Інакше,c в char
+	if(c)sum[sumIdx] = sumIdx  ? c : c + 48;
+	//перенесення залишкових цифр
+	char* rest; 
+	int restIdx;
+	if (aIdx > bIdx) {
+		rest = a;
+		restIdx = aIdx;
+	} else {
+		rest = b;
+		restIdx = bIdx;
+	}
+	while (restIdx > -1) {
+		sum[sumIdx--] += rest[restIdx--];
+	}
+	//фіксимо баг 
+	if (sum[0]==0) {
+		for (int i = 0; i < LEN ; sum[i] = sum[i + 1], i++);
+	}
+}
+int main() {
+
+	char a[LEN] = {0};
+	char b[LEN] = {0};
+	char c[LEN + 1] = {0};
+	cout << "enter the numbers" << endl;
+	cin >> a;
+	cin >> b;
+
+	sum(a, b, c);
+
+	cout << c << endl;
+	
+}
