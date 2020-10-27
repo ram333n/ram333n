e9c1b4efc5c64f5e9b056064b20f6f7c8318ca99
ram333n <71777440+ram333n@users.noreply.github.com>
2020-10-27 10:40:54 +0200

    Add files via upload

---
+++ lab_1_2_1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+using namespace std;
+double max(double a, double b) {
+	return ((a >= b) ? a : b);
+}
+double min(double a, double b) {
+	return ((a <= b) ? a : b);
+}
+void crossing() {
+	double x11, y11, x12, y12, x21, y21, x22, y22;
+	cout << "Enter the coords the tops of 1st segment" << endl;
+	cin >> x11 >> y11 >> x12 >> y12;
+	cout << "x11 = " << x11 << ", y11 = " << y11 << endl;
+	cout << "x12 = " << x12 << ", y12 = " << y12 << endl;
+	cout << "Enter the coords the tops of 2st segment" << endl;
+	cin >> x21 >> y21 >> x22 >> y22;
+	cout << "x21 = " << x21 << ", y21 = " << y21 << endl;
+	cout << "x22 = " << x22 << ", y22 = " << y22 << endl;
+	//визначаємо, як розміщені відрізки(1 випадок - розміщені на паралельних прямих)
+	if ((x11 == x12 && x21 == x22 && x11 != x21) 
+		|| (((y12 - y11) / (x12 - x11)) == ((y22 - y21) / (x22 - x21))) && (((x12 * y11 - y12 * x11) / (x12 - x11)) != ((x22 * y21 - y22 * x21) / (x22 - x21)))) {
+		cout << "There are no crossing (they are parallel)" << endl;
+	}
+
+	else if ((((y12 - y11) / (x12 - x11)) == ((y22 - y21) / (x22 - x21))) && (((x12 * y11 - y12 * x11) / (x12 - x11)) == ((x22 * y21 - y22 * x21) / (x22 - x21)))) {
+		//випадок, коли відрізки на одній прямій і кінець 1-го співпадає з початком 2-го і навпаки
+		if (((max(x11, x12) == min(x21, x22)))) {
+			cout << "The coords of crossing:x=" << x21 << " y=" << y21;
+		}
+		else if (((min(x11, x12) == max(x21, x22)))) {
+			cout << "The coords of crossing:x=" << x11 << " y=" << y11;
+		}
+		else {
+			cout << "Segments are have many common moments of no crossing" << endl;
+		}
+	}
+	//загальний випадок(відрізки не паралельні)
+	else {
+		double k1 = (y12 - y11) / (x12 - x11);
+		double k2 = (y22 - y21) / (x22 - x21);
+		double b1 = (x12 * y11 - y12 * x11) / (x12 - x11);
+		double b2 = (x22 * y21 - y22 * x21) / (x22 - x21);
+		double x, y;
+		x = (b2 - b1) / (k1 - k2);
+		y = k1 * x + b1;
+		//випадки, коли один з кут.коефіцієнтів =0
+		if (x11 == x12) {
+			x = x11;
+			y = k2 * x + b2;
+			if (min(x21, x22) <= x && x <= max(x21, x22)) {
+				cout << "The coords of crossing:x= " << x << "y=" << y << endl;
+			}
+			else {
+				cout << "There are no crossing" << endl;
+			}
+		}
+		else if (x21 == x22) {
+			    x = x21;
+				y = k1 * x + b1;
+				if (min(x11, x12) <= x && x <= max(x11, x12)) {
+					cout << "The coords of crossing:x= " << x << "y=" << y << endl;
+				}
+				else {
+					cout << "There are no crossing" << endl;
+				}
+		}
+		else if ((min(x11, min(x12, min(x21, x22))) <= x && x <= max(x11, max(x12, max(x21, x22))) && (min(y11, min(y12, min(y21, y22))) <= y&& y <= max(y11, max(y12, max(y21, y22)))))&&(min(x11,x12)<=x&&x<=max(x11,x12))&& (min(y11, y12) <= y&&y <= max(y11, y12))&& ((min(x21, x22) <= x && x <= max(x21, x22))) && ((min(y21, y22) <= y && y <= max(y21, y22)))) {
+			cout << "The coords of crossing:x= " << x << "y=" << y << endl;
+		}
+		else {
+			cout << "There are no crossing" << endl;
+
+		}
+	}
+}
+
+int main() {
+	do {
+		crossing();
+	} while (true);
+}
---
+++ lab_1_2_2.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdlib.h>//для функції rand()
+#include <conio.h>
+using namespace std;
+//зміна напрямку після зіткнення 2х точок
+void onIntersect(int i,int* x, int* y, int* dx, int* dy, 
+    int* direction, int* _x, double* k) {
+    double rad = atan(fabs(k[i]));
+    //дивимось, відносно якої осі потрібно поміняти напрямок
+    if (rad < -sqrt(3) || rad > sqrt(3)) {
+        k[i] = -k[i];
+    }
+    else {
+        //вектор напрямку
+        direction[i] = -direction[i];
+    }
+
+    dx[i] = x[i];
+    dy[i] = y[i];
+    _x[i] = 0;
+}
+//віддзеркалення відн Ox
+void onXIntersect(int i, int* x, int* y, int* dx, int* dy, int* direction, int* _x){
+        //вектор напрямку
+        direction[i] = -direction[i];
+        dx[i] = x[i];
+        dy[i] = y[i];
+        _x[i] = 0;
+}
+void onYIntersect(int i, int* x, int* y, int* dx, int* dy, double* k, int* _x) {
+    k[i] = -k[i];
+    dx[i] = x[i];
+    dy[i] = y[i];
+    _x[i] = 0;
+}
+int main()
+{
+    int n, w(200), h(200), STEP(1);
+    cin >> n;
+    //поточні координати точки та описи руху
+    int* x = new int[n];
+    int* y = new int[n];
+    //зміщення(для того, що описати рух після зіткнення)
+    int* dx = new int[n];
+    int* dy = new int[n];
+    //напрямок
+    int* direction = new int[n];
+    //координати точки в даний момент без зміщення
+    int* _x = new int[n] {0};
+    //кутовий коефіцієнт прямої, по якій іде рух
+    double* k = new double[n];
+    //зміння для того, щоб визначити, чи пропустити взаємодію
+    bool* cancelIntersect = new bool[n] {false};
+    //рухаємо всі точки
+    for (int i = 0; i < n; i++)
+    {
+        x[i] = dx[i] = rand() % w;
+        y[i] = dy[i] = rand() % h;
+        k[i] = ((double)(rand()) / RAND_MAX  * (rand() % 2 == 1 ? -1 : 1));
+        direction[i] = rand() % 2 == 1 ? -1 : 1;
+    }
+    //рухаємо всі точки
+    while (true) {
+        cout << "_________________________________" << endl;
+        for (int i = 0; i < n; i++) {
+            int anotherDot;
+            _x[i] += direction[i] * STEP;
+            y[i] = (int)(direction[i] * k[i] * _x[i]) + dy[i];
+            x[i] = dx[i] + _x[i];
+
+            if (cancelIntersect[i] == false) {
+                bool intersect = false;
+                for (int j = 0; j < n; j++) {
+                    //якщо точки частково перетинаються, то 2-га точка ігнорує перевірку на перетин і змінює свій напрям одразу
+                    if (j != i &&
+                        x[i] >= x[j] - STEP &&
+                        x[i] <= x[j] + STEP &&
+                        y[i] >= y[j] - STEP &&
+                        y[i] <= y[j] + STEP
+                        ) {
+                        anotherDot = j;
+                        cancelIntersect[j] = true;;
+                        onIntersect(j, x, y, dx, dy, direction, _x, k);
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (x[i] <= 0 || x[i] >= w) {
+                    while (x[i] <= 0) x[i]++;
+                    while (x[i] >= w) x[i]--;
+                    onXIntersect(i, x, y, dx, dy, direction, _x);
+                }
+                else if (y[i] <= 0 || y[i] >= h) {
+                    while (y[i] <= 0) y++;
+                    while (y[i] >= h) y--;
+                    onYIntersect(i, x, y, dx, dy, k, _x);
+                }
+                else if (intersect) {
+                    onIntersect(i, x, y, dx, dy, direction, _x, k);
+                    cout << "dot " << i << "(" << x[i] << "; " << y[i] << ")" << " intersected with dot " << anotherDot << " : (" << x[anotherDot] << ";" << y[anotherDot] << ")" << endl;
+                }
+            }
+            cancelIntersect[i] = false;
+            cout << "dot " << i << " : (" << x[i] << ";" << y[i] << ")" << endl;
+        }
+        //enter 
+        _getch();
+    }
+}
+
