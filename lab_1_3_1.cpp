a73ef7cc71c2586bf47c29fe04ae6efddde10a6f
ram333n <71777440+ram333n@users.noreply.github.com>
2020-11-08 22:38:10 +0200

    Add files via upload

---
+++ lab_1_3_1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+const double H = 0.000001;
+double Function(double x) {
+    return pow(x, 10) + pow(x, 9) + pow(x, 3) - x - 1;
+} 
+double DevOfFunc(double x, double(*func)(double)) {
+    return ((Function(x + H) - Function(x - H)) / (2 * H));
+}
+double SecondDevOfFunc(double x, double(*func12)(double)) {
+    return ((Function(x + H) + Function(x - H) - 2 * Function(x)) / (H * H));
+}
+
+int main()
+{
+    double a, b, x0,  eps;
+    cout << "Enter the a,b,eps (a<b)" << endl;
+    cin >> a >> b >> eps;
+    if (a >= b) cout << "incorrect input, try again";
+    else {
+        if (DevOfFunc(a, Function) * (SecondDevOfFunc(a, Function) > 0)) x0 = a;
+        else x0 = b;
+        do {
+            x0 = x0 - (Function(x0) / DevOfFunc(x0, Function));
+        } while (Function(x0) >= eps);
+        cout << "The root  : " << x0;
+    }
+}
---
+++ lab_1_3_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+const double EPS = 0.000001;
+const double STEP = EPS;
+const double H = (EPS) / 2;
+double Function(double x) {
+    return sin(x) * cos(3 * x);
+}
+
+double DevOfFunc(double x, double(*function)(double)) {
+    return ((Function(x + H) - Function(x - H)) / (2 * H));
+}
+
+bool isEqual(double a, double b, double eps) {
+    return fabs(a - b) <= eps;
+}
+
+double findPeriod(double(*function)(double)) {
+    double x = STEP;
+    double period = -1;
+    double fX = function(x);
+    double fXdx = DevOfFunc(x, function);
+    x += STEP;
+    for (long long i = 0; i < 10000000000; i++, x += STEP) {
+        if (isEqual(fX, function(x), EPS) && isEqual(fXdx, DevOfFunc(x, function), EPS) && isEqual(fX, function(2 * x), EPS) && isEqual(fXdx, DevOfFunc(2 * x , function), EPS)) {
+            period = x - STEP;
+            break;
+        }
+    }
+    return period;
+}
+int main()
+{
+    double period = findPeriod(Function);
+    if (period < 0) {
+        cout << "Function isn't periodic" << endl;
+    }
+    else {
+        cout << "Function is periodic, period :  " << period << endl;
+    }
+}
