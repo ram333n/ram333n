2e267e3fdb52d50207c5dd9a06104bf829346075
ram333n <71777440+ram333n@users.noreply.github.com>
2020-11-24 01:13:02 +0200

    Add files via upload

---
+++ Tasks_structandfiles.cpp
@@ -0,0 +1,208 @@
+﻿#include <iostream>
+#include <cstring>
+#include <fstream>
+#pragma warning(disable:4996)
+using namespace std;
+struct PRICE {
+    char NameOfStuff[20];
+    char NameOfShop[20];
+    float price;
+};
+
+bool IsStructEqual(PRICE p1, PRICE p2) {
+    if ((strcmp(p1.NameOfStuff , p2.NameOfStuff)==0) && (strcmp(p1.NameOfShop, p2.NameOfShop) == 0) && (p1.price == p2.price)) {
+        return true;
+    }
+    return false;
+}
+
+void PrintStruct(struct PRICE p) {
+    cout << "....................................." << endl;
+    cout << "Name of shop : " << p.NameOfShop << endl;
+    cout << "Name of stuff : " << p.NameOfStuff << endl;
+    cout << "Price : " << p.price << endl;
+    cout << "....................................." << endl;
+}
+
+void sort(PRICE* arr)
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 6 - i; j++)
+        {
+            if (strcmp(arr[j].NameOfStuff, arr[j + 1].NameOfStuff) > 0) {
+                PRICE temp;
+                temp = arr[j];
+                arr[j + 1] = arr[j];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void fillIn(PRICE* arr) {
+    cout << "Fill in the array" << endl;
+    for (int i = 0; i < 8; i++) {
+        while (true) {
+            bool IsCopy = false;
+            bool good = true;
+            cout << "Enter the name of shop" << endl;
+            cin >> arr[i].NameOfShop;
+            cout << "Enter the name of stuff" << endl;
+            cin >> arr[i].NameOfStuff;
+            cout << "Enter the price" << endl;
+            do
+            {
+                cin >> arr[i].price;
+                if (!(good = cin.good())||!(good=(arr[i].price>0)))
+                    cout << "Incorrect input" << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            } while (!good);
+            for (int j = i-1; j >= i-1; j--) {
+                if (IsStructEqual(arr[i], arr[j])) {
+                    cout << "This is a copy of note, try again" << endl;
+                    IsCopy = true;
+                    break;
+                }
+            }
+            if (!IsCopy) {
+                break;
+            }
+        }
+        PrintStruct(arr[i]);
+    }
+    sort(arr);
+}
+
+void Read(PRICE* arr) {
+    char stuff[20];
+    cout << "Enter the name of stuff" << endl;
+    cin >> stuff;
+    bool IsInArray = false;
+    for (int i = 0; i < 8; i++) {
+        if (strcmp(stuff, arr[i].NameOfStuff) == 0) {
+            PrintStruct(arr[i]);
+            IsInArray = true;
+        }
+    }
+    if (!IsInArray) {
+        cout << "Stuff isn't available at the shops" << endl;
+    }
+}
+void Update(PRICE* arr) {
+    PRICE oldInfo;
+    PRICE newInfo;
+    bool good = true;
+    bool IsInArray = false;
+    cout << "Enter the information about stuff" << endl;
+    cin >> oldInfo.NameOfShop >> oldInfo.NameOfStuff >> oldInfo.price;
+    cout << "Enter the new information about stuff" << endl;
+    cin >> newInfo.NameOfShop >> newInfo.NameOfStuff;
+    do
+    {
+        cin >> newInfo.price;
+        if (!(good = cin.good()) || !(good = (newInfo.price > 0)))
+            cout << "Incorrect input" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    } while (!good);
+    for (int i = 0; i < 8; i++) {
+        if (IsStructEqual(arr[i], oldInfo)) {
+            IsInArray = true;
+                arr[i] = newInfo;
+                sort(arr);
+                break;
+        }
+    }
+    if (!IsInArray) {
+        cout << "Enter the right info" << endl;
+    }
+}
+void Dump(PRICE* arr) {
+    for (int i = 0; i < 8; i++) {
+        PrintStruct(arr[i]);
+    }
+}
+void Export(PRICE* arr) {
+    char repos[90] = "C:\\Users\\user\\source\\repos\\Tasks_univer\\";
+    char fileName[20];
+    cout << "Enter the name of file with format(for example: file.txt)" << endl;
+    cin >> fileName;
+    ofstream file(fileName);
+    FILE* f = fopen(strcat(repos, fileName), "w");
+    for (int i = 0; i < 8; i++) {
+        file << "............................" << endl;
+        file << arr[i].NameOfShop << endl;
+        file << arr[i].NameOfStuff << endl;
+        file << arr[i].price << endl;
+    }
+    file.close();
+    cout << "The repository of the file : C:\\Users\\user\\source\\repos\\Tasks_univer" << endl;
+}
+
+void Import(PRICE* arr) {
+    char fileName[20];
+    cout << "Enter the name of file (for example: file.txt)" << endl;
+    cin >> fileName;
+    ifstream file(fileName);
+    if (!file.is_open()) cout << "File doesn't exist" << endl;
+    else {
+        for (int i = 0; i<8; i++)
+        {
+            file.getline(arr[i].NameOfShop, 30);
+            file.getline(arr[i].NameOfStuff, 30);
+            (file >> arr[i].price).get();
+        }
+    }
+    sort(arr);
+} 
+
+int main()
+{
+    int Switcher;
+    PRICE array[8];
+    cout << "1.Import:" << endl;
+    cout << "2.FillIn" << endl;
+    cin >> Switcher;
+    switch (Switcher) {
+    case 1:
+        Import(array);
+        break;
+    case 2:
+        fillIn(array);
+        break;
+    default:
+        cout << "Enter the right action" << endl;
+    }
+    cout << "1:Read" << endl;
+    cout << "2:Update" << endl;
+    cout << "3:Dump" << endl;
+    cout << "4:Export" << endl;
+    cout << "5:Exit" << endl;
+    bool loop = true;
+    while (loop) {
+        cin >> Switcher;
+        switch (Switcher)
+        {
+        case 1:
+            Read(array);
+            break;
+        case 2:
+            Update(array);
+            break;
+        case 3:
+            Dump(array);
+            break;
+        case 4:
+            Export(array);
+            break;
+        case 5:
+            loop = false;
+            break;
+        default:
+            cout << "Enter the right action" << endl;
+        }
+    }
+
+}
---
+++ lab_5.cpp
@@ -0,0 +1,291 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+using namespace std;
+
+template<typename T> struct Stack {
+    T value;
+    struct Stack* next;
+};
+
+template<typename T> void push(Stack<T>*& TOP, T VALUE) {
+    Stack<T>* top = new Stack<T>;
+    top->value = VALUE;
+    top->next = TOP != NULL ? TOP : NULL;
+    TOP = top;
+}
+
+template<typename T> T pop(Stack<T>*& TOP) {
+    if (TOP == NULL) {
+        return NULL;
+    }
+    T value = TOP->value;
+    Stack<T>* oldTop = TOP;
+    TOP = oldTop->next;
+    delete oldTop;
+    return value;
+}
+
+
+void substring(char* str, char* numStr, int s, int e) {
+    for (int i = s, j = 0; i < e; i++, j++) {
+        numStr[j] = str[i];
+    }
+}
+
+
+double ApplyOperation(double op1, char op, double op2 = 0) {
+
+    switch (op) {
+    case '+': {
+        return op1 + op2;
+    }
+    case '-': {
+        return op1 - op2;
+    }
+    case '/': {
+        return op1 / op2;
+
+    }
+    }
+    return 0;
+}
+
+void calcRPN(Stack<double>*& stack, char* problem, int& i) {
+
+    bool isPrefix = false;
+    bool skip = false;
+    char prefix[2]{ 0 };
+
+    while (problem[i] != 0) {
+
+        switch (problem[i]) {
+        case ')': {
+            i++;
+            return;
+        }
+        case '(': {
+            i++;
+            calcRPN(stack, problem, i);
+            break;
+        }
+        case '+':
+        case '-': {
+
+            int j = i - 1;
+            while (j >= 0 && problem[j] == ' ') j--;
+
+            if (j < 0 || !isdigit(problem[j])) {
+                prefix[0] = problem[i];
+                isPrefix = true;
+                skip = true;
+            }
+            else {
+
+                double op2 = pop(stack);
+                double op1 = pop(stack);
+                double res = ApplyOperation(op1, problem[i], op2);
+                push(stack, res);
+            }
+            i++;
+            break;
+        }
+        case '/': {
+            double op2 = pop(stack);
+            double op1 = pop(stack);
+            double res = ApplyOperation(op1, problem[i], op2);
+            push(stack, res);
+            i++;
+            break;
+        }
+        case ' ': {
+            i++;
+            break;
+        };
+        default: {
+            if (!skip) {
+                int start = i;
+
+                while (isdigit(problem[i])) {
+                    i++;
+                }
+                char* numStr = new char[i - start]{ 0 };
+                substring(problem, numStr, start, i);
+                double num = atof(numStr);
+                if (isPrefix && prefix[0] == '-') num *= -1;
+                push(stack, num);
+                isPrefix = false;
+                delete[] numStr;
+            }
+            else {
+                skip = false;
+            }
+        }
+        }
+    }
+}
+
+bool hasHigherPriority(char op1, char op2) {
+    if ((op1 == '+' || op1 == '-') && (op2 == '+' || op2 == '-')) {
+        return false;
+    };
+    return op1 > op2;
+}
+
+
+
+bool isOperation(char* str) {
+    return
+        strcmp(str, "exp") == 0 ||
+        strcmp(str, "+") == 0 ||
+        strcmp(str, "-") == 0 ||
+        strcmp(str, "/") == 0;
+}
+
+int toRPN(char* problem, char* rpn) {
+    int i = 0;
+    Stack<char*>* stack = NULL;
+
+    bool isPrefix = false;
+    bool skip = false;
+    char prefix[2]{ 0 };
+
+    while (problem[i] != 0) {
+
+        if (problem[i] == 'e' && problem[i + 1] == 'x' && problem[i + 2] == 'p') {
+            char EXP[] = "exp";
+            push(stack, EXP);
+            i += 3;
+        }
+        else {
+            switch (problem[i]) {
+            case ')': {
+                while (stack != NULL && strcmp(stack->value, "(") != 0) {
+                    char* str = pop(stack);
+                    strcat(rpn, str);
+                    strcat(rpn, " ");
+                }
+                if (stack == NULL) {
+                    return -1;
+                }
+                pop(stack);
+                i++;
+                break;
+            }
+            case '(': {
+                char arr[2]{ '(', 0 };
+                push(stack, arr);
+                i++;
+                break;
+            }
+            case '+':
+            case '-': {
+
+                int j = i - 1;
+                while (j >= 0 && problem[j] == ' ') j--;
+
+                if (j < 0 || !isdigit(problem[j])) {
+                    prefix[0] = problem[i];
+                    isPrefix = true;
+                    skip = true;
+                }
+                else {
+
+                    while (stack != NULL && isOperation(stack->value) && hasHigherPriority(stack->value[0], problem[i])) {
+                        char* str = pop(stack);
+                        strcat(rpn, str);
+                        strcat(rpn, " ");
+                    }
+                    char arr[2]{ problem[i], 0 };
+                    push(stack, arr);
+                }
+                i++;
+                break;
+            }
+            case '/': {
+
+                while (stack != NULL && isOperation(stack->value) && hasHigherPriority(stack->value[0], problem[i])) {
+                    char* str = pop(stack);
+                    strcat(rpn, str);
+                    strcat(rpn, " ");
+                }
+                char arr[2]{ problem[i], 0 };
+                push(stack, arr);
+                i++;
+                break;
+            }
+            case ' ': {
+                continue;
+            };
+            default: {
+
+                if (!skip) {
+                    int start = i;
+
+                    char* numStr;
+
+                    if (isalpha(problem[i])) {
+                        while (isalpha(problem[i]) || isdigit(problem[i])) {
+                            i++;
+                        }
+                        numStr = new char[i - start + 1]{ 0 };
+                        substring(problem, numStr, start, i);
+                        char num[50];
+                        cout << "enter value of variable " << numStr << endl;
+                        cin.getline(num, 50);
+                        strcat(rpn, num);
+                        strcat(rpn, " ");
+                    }
+                    else {
+                        while (isdigit(problem[i])) {
+                            i++;
+                        }
+                        numStr = new char[i - start + 1]{ 0 };
+                        substring(problem, numStr, start, i);
+                        if (isPrefix) strcat(rpn, prefix);
+                        strcat(numStr, " ");
+                        strcat(rpn, numStr);
+                    }
+
+
+                    isPrefix = false;
+                }
+                else {
+                    skip = false;
+                }
+                //delete[] numStr;
+            }
+            }
+        }
+    }
+
+    while (stack != NULL) {
+        char* op = pop(stack);
+        if (!isOperation(op)) return -1;
+        strcat(rpn, op);
+        strcat(rpn, " ");
+    }
+
+    return 0;
+}
+
+
+int main() {
+
+
+
+    char problem[200] = "(-1+a)/4+1";
+    //char problem[200];
+    //cout << "enter problem" << endl;
+    //cin.getline (problem,200);
+    char rpn[200]{ 0 };
+    toRPN(problem, rpn);
+    cout << "RPN notation " << rpn << endl;
+    Stack<double>* stack = NULL;
+    int i = 0;
+    calcRPN(stack, rpn, i);
+    cout << stack->value << endl;
+
+}
